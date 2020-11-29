2c1d0ef89b33f7274f21e2a71a47a1409e42ae4b
昊 王 <1721110240@qq.com>
2020-11-29 22:51:50 +0800

    Steven

---
+++ TestJWT/AuthenticationCenter/Controllers/AuthenticationController.cs
@@ -0,0 +1,61 @@
+﻿using AuthenticationCenter.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationCenter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : Controller
+    {
+        #region MyRegion
+        private ILogger<AuthenticationController> _logger = null;
+        private IJWTService _iJWTService = null;
+        private readonly IConfiguration _iConfiguration;
+        public AuthenticationController(ILoggerFactory factory,
+            ILogger<AuthenticationController> logger,
+            IConfiguration configuration
+            , IJWTService service)
+        {
+            this._logger = logger;
+            this._iConfiguration = configuration;
+            this._iJWTService = service;
+        }
+        #endregion
+        [Route("Get")]
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            return new List<int>() { 1, 2, 3, 4, 6, 7 };
+        }
+
+        [Route("Login")]
+        [HttpPost]
+        public string Login(string name, string password)
+        {
+            if ("2".Equals(name) && "123456".Equals(password))
+            {
+                string token = this._iJWTService.GetToken(name);
+                return JsonConvert.SerializeObject(new
+                {
+                    result = true,
+                    token
+                });
+            }
+            else
+            {
+                return JsonConvert.SerializeObject(new
+                {
+                    result = false,
+                    token = ""
+                });
+            }
+        }
+    }
+}
---
+++ TestJWT/Demo/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Microsoft.AspNetCore.Authorization.Authorize]
+    public class UserController : Controller
+    {
+        private List<Users> _userList = null;
+        public UserController() 
+        {
+            this._userList = new List<Users> { new Users { UserID = 1, UserEmail = "121221@qq.com", UserName = "张三" } };
+        }
+        [HttpGet]
+        [Microsoft.AspNetCore.Authorization.AllowAnonymous]
+        public IEnumerable<Users> Get()
+        {
+            return _userList;
+        }
+        [HttpPost]
+        public IEnumerable<Users> GetUserByID(int id)
+        {
+            return _userList;
+        }
+
+    }
+    public class Users
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+    }
+}
